9623ea3b28db094110469e96b5675d9cc26ecbf8
Narongchaiv2 <nrngkhchaykaewkanha@gmail.com>
2026-03-10 15:57:29 +0700

    v1

---
+++ app/src/main/java/com/pu/narongchaiv2/GBValues.java
@@ -0,0 +1,5 @@
+package com.pu.narongchaiv2;
+
+public class GBValues {
+    public static String hostname = "http://localhost/narongchaiv1/";
+}
--- app/src/main/java/com/pu/narongchaiv2/Member.java
+++ app/src/main/java/com/pu/narongchaiv2/Member.java
@@ -1,3 +1,5 @@
+package com.pu.narongchaiv2;
+
 import java.io.Serializable;
 public class Member implements Serializable {
     private String id;
