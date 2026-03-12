6a4e8bf807697cec1348eec1ae8f4f4deb72664c
Narongchaiv2 <nrngkhchaykaewkanha@gmail.com>
2026-03-12 10:48:17 +0700

    v3

--- app/src/main/java/com/pu/narongchaiv2/GBValues.java
+++ app/src/main/java/com/pu/narongchaiv2/GBValues.java
@@ -1,5 +1,5 @@
 package com.pu.narongchaiv2;
 
 public class GBValues {
-    public static String hostname = "http://localhost/narongchaiv1/";
+    public static String hostname = "http://10.30.200.160/narongchaiv1/";
 }
